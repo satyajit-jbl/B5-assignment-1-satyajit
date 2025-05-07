2a84c0399121242101132ed4a79d3e76b5643ec8
satyajit <satyajit_jbl@yahoo.com>
2025-05-07 22:58:49 +0600

    problem 2 & 3 done

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function formatString(input, toUpper) {
 console.log(formatString("Hello"));
 console.log(formatString("Hello", true));
 console.log(formatString("Hello", false));
+function filterByRating(items) {
+    // return items.filter(item=>item.rating>=5);
+    items.forEach(item => {
+        if (item.rating >= 4)
+            return items;
+    });
+    return items;
+}
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+const result = filterByRating(books);
+console.log(result);
+function concatenateArrays(...arrays) {
+    let result = [];
+    for (const arr of arrays) {
+        result = [...result, ...arr];
+    }
+    return result;
+}
+console.log(concatenateArrays(["a", "b"], ["c"]));
+console.log(concatenateArrays([1, 2], [3, 4], [5]));
--- index.ts
+++ index.ts
@@ -5,4 +5,34 @@ function formatString(input: string, toUpper?: boolean): string {
   console.log(formatString("Hello"));
   console.log(formatString("Hello", true));
   console.log(formatString("Hello", false));
+
+
+  function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    // return items.filter(item=>item.rating>=5);
+    items.forEach(item => {
+        if (item.rating>=4) return items
+    });
+    return items
+  }
+
+  const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+  ];
+
+  const result = filterByRating(books);
+  console.log(result);
+
+  function concatenateArrays<T>(...arrays: T[][]): T[]{
+    let result : T[] = [];
+    for(const arr of arrays){
+        result=[...result, ...arr];
+    }
+    return result
+  }
+
+  console.log(concatenateArrays(["a", "b"], ["c"]));
+  console.log(concatenateArrays([1, 2], [3, 4], [5]));
+  
   
